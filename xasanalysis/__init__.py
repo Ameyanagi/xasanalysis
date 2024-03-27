438f1f18509d972fbe8140b335e918458210bd02
Ameyanagi <ryuichi@ameyanagi.com>
2024-03-27 17:29:14 -0400

    add functions to __init.py

--- xasanalysis/__init__.py
+++ xasanalysis/__init__.py
@@ -1,5 +1,16 @@
 """Top-level package for XASAnalysis."""
 
 __author__ = """Ameyangi"""
-__email__ = 'contact@ameyanagi.com'
-__version__ = '0.1.0'
+__email__ = "contact@ameyanagi.com"
+__version__ = "0.1.0"
+
+
+from .xasanalysis import (
+    read_xmu,
+    read_QAS_transmission,
+    read_QAS_fluorescence,
+    read_QAS_ref,
+    read_QAS_SDD,
+    calc_shift,
+    XASAnalysis,
+)
